f0928afe4f9988f06d8bd04e96452dfca7b5560c
gregelgin <gselgin@uvm.edu>
2020-01-31 09:02:11 -0500

    Update 5

--- GameStatus.java
+++ GameStatus.java
@@ -1,3 +1,7 @@
+// Greg Elgin
+// Last updated: 1/31/20
+// Game Status enumerator to be used in Snake Game
+
 public enum GameStatus {
     RUNNING,
     GAME_OVER
--- ImageLoader.java
+++ ImageLoader.java
@@ -1,6 +1,6 @@
 // Greg Elgin
-// 12/16/19
-// Loads the images into buffered images
+// 1/31/20
+// Loads images into buffered images to be used by UIManager
 // Source: http://gamecodeschool.com/android/building-a-simple-game-engine/
 // Source: https://github.com/ahmetcandiroglu/Super-Mario-Bros
 
@@ -11,24 +11,17 @@ import java.io.IOException;
 
 public class ImageLoader {
 
-    // Construct ImageLoader, initially is empty
-    public ImageLoader() {
-
-    }
-
-    // takes image path as parameter and returns buffered image
+    // Takes image path as parameter and returns buffered image
+    // Catches exceptions and continues game with files found
     public BufferedImage loadImage(String path) {
         BufferedImage imageToReturn = null;
 
         try {
             imageToReturn = ImageIO.read(getClass().getResource(path));
         }
-        catch (IOException e) {
-            System.out.println("File not found");
+        catch (IOException | IllegalArgumentException e) {
+            System.out.println(path + " file not found");
         }
         return imageToReturn;
     }
-
-
-
 }
